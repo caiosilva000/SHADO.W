02f8a98ce72e74fb85f06667f91801dcafc8b5e0
Caio Silva <caiotbr@gmail.com>
2023-03-06 16:46:19 +0100

    scaffold article

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_153818) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_154530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
