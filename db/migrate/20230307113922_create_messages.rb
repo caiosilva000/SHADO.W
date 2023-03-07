1e4399e9f5ba13e6fb3e1ef2c3d73d9d81a11c14
Caio Silva <caiotbr@gmail.com>
2023-03-07 17:56:19 +0100

    seeds

--- db/migrate/20230307113922_create_messages.rb
+++ db/migrate/20230307113922_create_messages.rb
@@ -1,4 +1,4 @@
-class CreateMessages < ActiveRecord::Migration[7.0]
+t class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
       t.references :user, null: false, foreign_key: true
