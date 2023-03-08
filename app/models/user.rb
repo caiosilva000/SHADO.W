26fa62da961e69d987d5943d2b58f16a60fea31c
Caio Silva <caiotbr@gmail.com>
2023-03-08 17:41:49 +0100

    AUTH

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,15 +12,21 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
 
-    def self.from_omniauth(access_token)
-      data = access_token.info
-      user = User.where(email: data['email']).first
-      unless user
-          user = User.create(
-             email: data['email'],
-             password: Devise.friendly_token[0,20]
-          )
-      end
-      user
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+
+    # Fetch the profile picture if available and save it to the user's profile_pic attribute
+    if access_token.info.image.present?
+      user.update(profile_pic: access_token.info.image)
+    end
+
+    user
   end
 end
