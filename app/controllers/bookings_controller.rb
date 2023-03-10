51f25f8fe708eb6c35da990854bd2c5d322021b4
Jannis Weber <jannis.weber@gmx.de>
2023-03-10 14:03:59 +0100

    probably half there

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @bookee = Bookee.find(params[:bookee_id])
+    @booking.bookee = @bookee
+
+    if @booking.save!
+      redirect_to my_profile_booking_path(@bookee, @booking)
+    else
+      render :new
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_084042) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_10_083829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,10 +92,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_084042) do
     t.datetime "updated_at", null: false
     t.string "github_uid"
     t.string "access_token"
-    t.string "github_nickname"
+    t.string "  "
     t.integer "contributions"
-    t.string "profile_pic"
     t.string "user_name"
+    t.string "profile_pic"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
