572a23bd40bd084580f899295ae33e8b39d64aed
Caio Silva <caiotbr@gmail.com>
2023-03-12 14:54:39 +0100

    Side menu added with a slide in effect

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,7 @@
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "stimulus/webpclearack-helpers"
-import * as Turbo from "@hotwired/turbo"
-const application = Application.start()
-// const context = require.context("./controllers", true, /\.js$/)
-// application.load(definitionsFromContext(context))
 
-window.Turbo = Turbo
+const application = Application.start()
+
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
