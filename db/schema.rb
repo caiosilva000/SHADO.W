02cfae42037df673d066b7e406fba183e55a972d
Caio Silva <caiotbr@gmail.com>
2023-03-13 16:56:25 +0100

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_131206) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_154325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -97,6 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_131206) do
     t.string "user_name"
     t.string "profile_pic"
     t.string "top_languages", default: [], array: true
+    t.boolean "senior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
