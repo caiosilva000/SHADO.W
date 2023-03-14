640a5961c418994ebb730085fe7aa411c887d314
martin siegert <oddnutshell@aol.com>
2023-03-14 12:18:44 +0100

    added bio migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_154325) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_111151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,6 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_154325) do
     t.string "profile_pic"
     t.string "top_languages", default: [], array: true
     t.boolean "senior", default: false
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
