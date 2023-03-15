6123e7d5bd4b81eaf373a51c12a90423dba5dfc3
Caio Silva <caiotbr@gmail.com>
2023-03-15 09:37:19 +0100

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,38 @@
 Rails.application.routes.draw do
-  # Root path
+  get 'users/show'
+  resources :rooms do
+    resources :messages
+  end
   root to: "pages#index"
-
-  # Devise routes
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
-  # Custom devise routes
   devise_scope :user do
-    # Custom sign in route
     get 'users', to: 'devise/sessions#new'
   end
 
-  # Github OAuth callback route
-  get '/auth/:provider/callback', to: 'github_auth#github'
-
-  # Nested user routes
-  resources :users, only: [:show] do
-    # Nested booking routes
+  resources :users, only: [] do
     resources :bookings, only: [:new, :create]
-    # Nested follow routes
-    resources :follows, only: [:create]
   end
-
-  # Booking member route for thank you page
+  resources :availabilities, only: [:new, :create]
+  get '/users', to: 'pages#index'
+  get '/auth/:provider/callback', to: 'github_auth#github'
+  get '/my_profile', to: 'pages#my_profile', as: 'my_profile'
+  get '/my_settings', to: 'pages#my_settings', as: 'my_settings'
+  get '/my_bookings', to: 'pages#my_bookings', as: 'my_bookings'
+  get '/users/:id', to: 'users#show', as: 'user'
   resources :bookings, only: [] do
     member do
       get 'thank_you'
     end
   end
 
-  # Availability routes
-  resources :availabilities, only: [:new, :create]
+  resources :users, only: [] do
+    resources :follows, only: [:create]
+  end
 
-  # Nested chatroom routes
   resources :chatrooms, only: :show do
-    # Nested message routes
     resources :messages, only: :create
   end
+  resources :follows, only: ["destroy"]
+  mount ActionCable.server => '/cable'
 
-  # Nested room routes
-  resources :rooms do
-    # Nested message routes
-    resources :messages, module: :rooms
-  end
-
-  # Follows destroy route
-  resources :follows, only: [:destroy]
-
-  # Custom pages routes
-  get '/users', to: 'pages#index'
-  get '/my_profile', to: 'pages#my_profile', as: 'my_profile'
-  get '/my_settings', to: 'pages#my_settings', as: 'my_settings'
-  get '/my_bookings', to: 'pages#my_bookings', as: 'my_bookings'
 end
