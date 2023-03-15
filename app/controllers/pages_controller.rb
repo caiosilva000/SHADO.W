a6f7420c7172aa106c38de0cd1ca276fbcc5871d
martin siegert <oddnutshell@aol.com>
2023-03-15 12:03:07 +0100

    index added languages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,7 @@ class PagesController < ApplicationController
       render :new
     end
   end
-  
+
   def my_profile
     @user = current_user
     @fa_map = {
@@ -145,6 +145,60 @@ class PagesController < ApplicationController
   end
 
   def index
+    @fa_map = {
+      'Ruby' => 'fa-sharp fa-regular fa-gem',
+      'Shell' => 'fas fa-terminal',
+      'Vue' => 'fab fa-vuejs',
+      'ActionScript' => 'fab fa-ioxhost',
+      'Ada' => 'fas fa-braille',
+      'AppleScript' => 'fab fa-apple',
+      'ASP.NET' => 'fas fa-code',
+      'Assembly' => 'fas fa-cogs',
+      'Awk' => 'fas fa-code-branch',
+      'Bash' => 'fas fa-terminal',
+      'C' => 'fas fa-code',
+      'C#' => 'fab fa-windows',
+      'C++' => 'fab fa-cuttlefish',
+      'Clojure' => 'fab fa-leanpub',
+      'CoffeeScript' => 'fab fa-simplybuilt',
+      'CSS' => 'fab fa-css3-alt',
+      'D' => 'fab fa-d-and-d',
+      'Dart' => 'fab fa-dart',
+      'Elixir' => 'fab fa-phoenix-framework',
+      'Elm' => 'fab fa-elm',
+      'Erlang' => 'fab fa-erlang',
+      'F#' => 'fas fa-code',
+      'Fortran' => 'fas fa-braille',
+      'Go' => 'fab fa-google',
+      'Groovy' => 'fab fa-groovy',
+      'Haskell' => 'fab fa-haskell',
+      'HTML' => 'fab fa-html5',
+      'Java' => 'fab fa-java',
+      'JavaScript' => 'fab fa-js-square',
+      'Julia' => 'fab fa-julia',
+      'Kotlin' => 'fab fa-kotlin',
+      'Lua' => 'fab fa-lua',
+      'MATLAB' => 'fas fa-square',
+      'Objective-C' => 'fab fa-apple',
+      'OCaml' => 'fas fa-code-branch',
+      'Perl' => 'fab fa-perl',
+      'PHP' => 'fab fa-php',
+      'PowerShell' => 'fas fa-terminal',
+      'Python' => 'fab fa-python',
+      'R' => 'fab fa-r-project',
+      'Rust' => 'fab fa-rust',
+      'Scala' => 'fab fa-scala',
+      'Scheme' => 'fas fa-code',
+      'SQL' => 'fas fa-database',
+      'Swift' => 'fab fa-swift',
+      'TypeScript' => 'fab fa-js-square',
+      'VB.NET' => 'fab fa-windows',
+      'Vim script' => 'fab fa-vimeo-v',
+      'Visual Basic' => 'fab fa-vnv',
+      'WebAssembly' => 'fas fa-code',
+      'XML' => 'fas fa-code',
+      'XSLT' => 'fas fa-code'
+    }
     if params[:query].present?
       sql_query = "user_name ILIKE :query OR github_nickname ILIKE :query"
       @users = User.where(sql_query, query: "%#{params[:query]}%")
